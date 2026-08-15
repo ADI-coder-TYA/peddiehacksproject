bc653ec902a056d8765eedf4daac15e21d049694
ADI-coder-TYA <thisisrealadi@gmail.com>
2026-08-15 15:12:01 +0530

    fix(patient): add missing api_config import to patient main screen

--- intellidesk_app/lib/screens/patient/patient_main_screen.dart
+++ intellidesk_app/lib/screens/patient/patient_main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import '../../config/api_config.dart';
 import '../../models/user_role.dart';
 import '../../providers/auth_provider.dart';
 import '../../providers/ticket_provider.dart';
